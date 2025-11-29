d82e640228edaa4adda132700aac57e003c0a425
Simon Rothman <srothman@mit.edu>
2025-11-29 12:42:45 -0500

    Runs as standalone

--- SimTruth/python/SimClusterTable_cfi.py
+++ SimTruth/python/SimClusterTable_cfi.py
@@ -15,8 +15,8 @@ SimClusterTable = cms.EDProducer(
         pdgId = Var('pdgId()', 'int', doc='PDG ID of the simcluster'),
         threeCharge = Var('threeCharge()', 'int', doc='3*charge of the simcluster'),
         nSimHits = Var('numberOfRecHits', 'int', doc='number of simhits in the simcluster'),
-        hasHGCALHit = Var('hasHGCALHit()', 'bool', doc='whether the simcluster has a HGCAL hit'),
-        allHitsHGCAL = Var('allHitsHGCAL()', 'bool', doc='whether all hits in the simcluster are HGCAL hits'),
+        #hasHGCALHit = Var('hasHGCALHit()', 'bool', doc='whether the simcluster has a HGCAL hit'),
+        #allHitsHGCAL = Var('allHitsHGCAL()', 'bool', doc='whether all hits in the simcluster are HGCAL hits'),
         simEnergy = Var('simEnergy()', 'float', doc='simulated energy of the simcluster'),
     )
 )
